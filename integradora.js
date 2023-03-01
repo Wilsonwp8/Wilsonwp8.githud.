37b2996b162093d70fec2ede0ded779f51b01a58
Wilsonwp8 <126630329+Wilsonwp8@users.noreply.github.com>
2023-02-28 22:41:37 -0300

    Add files via upload

---
+++ integradora.js
@@ -0,0 +1,12 @@
+
+document.getElementById('boton_default 1').addEventListener('click', function() {
+    document.body.style.backgroundColor = 'grey';
+});
+
+document.getElementById('boton_default 2').addEventListener('click', function() {
+    document.body.style.backgroundColor = 'yellow';
+});
+
+document.getElementById('boton_default 3').addEventListener('click', function() {
+    document.body.style.backgroundColor = 'white';
+});
